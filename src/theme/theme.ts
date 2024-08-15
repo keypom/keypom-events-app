7e177077515e9ec4b02a1f7a8bd390efa615ee01
Zeeshan Ahmad <itexpert120@outlook.com>
2024-08-15 22:04:05 +0500

    Use pnpm

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,17 +1,7 @@
-import { extendTheme, type ThemeConfig } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
 
-const colors = {
-  brand: {
-    900: "#1a365d",
-    800: "#153e75",
-    700: "#2a69ac",
-  },
-};
-
-const config: ThemeConfig = {
-  initialColorMode: "dark",
-  useSystemColorMode: false,
-};
+import { colors } from "./colors";
+import { config } from "./config";
 
 export const theme = extendTheme({
   colors,
