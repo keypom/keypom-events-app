5a18369cd459ea519d02bc3fe7adc33d849631f8
elliotBraem <elliotBraem@users.noreply.github.com>
2024-08-20 22:51:43 +0000

    Apply formatting changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
+import { nodePolyfills } from "vite-plugin-node-polyfills";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-  // resolves polyfills needed by near-api-js
-  nodePolyfills({ include: ['http', 'https', 'buffer'] }),],
+  plugins: [
+    react(),
+    // resolves polyfills needed by near-api-js
+    nodePolyfills({ include: ["http", "https", "buffer"] }),
+  ],
   server: {
     port: 5173,
   },
