4f9ab3dc36792bd015bc6a8249faea09817e9cf7
Zeeshan Ahmad <itexpert120@outlook.com>
2024-08-29 22:16:47 +0500

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from "vite";
 
 // custom plugins
 import { nodePolyfills } from "vite-plugin-node-polyfills";
-import { VitePWA } from "vite-plugin-pwa";
+// import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
