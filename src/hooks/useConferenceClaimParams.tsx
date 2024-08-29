44ddcff813a20e3c384ac8cbc117e53279d79371
Elliot Braem <16282460+elliotBraem@users.noreply.github.com>
2024-08-29 00:22:14 -0400

    redirect to me page

--- src/hooks/useConferenceClaimParams.tsx
+++ src/hooks/useConferenceClaimParams.tsx
@@ -14,7 +14,7 @@ export const useConferenceClaimParams = () => {
     localStorage.setItem("EVENT_SECRET_KEY", secretKey);
 
     // Clear URL
-    navigate(`/conference/app/profile`, { replace: true });
+    navigate(`/me`, { replace: true });
   } else {
     // Fallback to local storage
     dropId = localStorage.getItem("EVENT_DROP_ID") || "";
