594552f35afc159925d16ac12a68212a52d69120
Elliot Braem <16282460+elliotBraem@users.noreply.github.com>
2024-09-05 18:40:12 -0400

    add receive

--- src/routes/wallet/receive.tsx
+++ src/routes/wallet/receive.tsx
@@ -1,43 +1,49 @@
-import { Heading, Text, VStack } from "@chakra-ui/react";
+import { Flex, Heading, Text, VStack } from "@chakra-ui/react";
 import QRCode from "react-qr-code";
 
 import { PageHeading } from "@/components/ui/page-heading";
+import { useAccountData } from "@/hooks/useAccountData";
 
 export default function Receive() {
-  const account = {
-    account_id: "anybody.testnet", // TODO: need to modify
-    display_name: "No Name Account", // TODO: need to modify
-  };
+  const { data } = useAccountData();
+
+  const { accountId, displayName } = data!;
+  const qrValue = `${window.origin}/wallet/send?to=${accountId}`;
 
   return (
     <VStack p={4}>
       <PageHeading title="Receive" showBackButton />
-      <VStack p={4}>
-        <QRCode
-          value={account.account_id}
-          bgColor="transparent"
-          fgColor="var(--chakra-colors-brand-400)"
-        />
-        <VStack alignItems="center" p={4}>
-          <Heading
-            as="h3"
-            fontSize="2xl"
-            textAlign={"center"}
-            color="white"
-            noOfLines={1}
-          >
-            {account.display_name || "No Name Account"}
-          </Heading>
-          <Text
-            fontFamily="mono"
-            color="brand.400"
-            textAlign={"center"}
-            noOfLines={1}
-          >
-            {account.account_id}
-          </Text>
-        </VStack>
-      </VStack>
+        <Flex
+          align="center"
+          flexDir="column"
+          p={{ base: "6", md: "8" }}
+          pt={{ base: "12", md: "16" }}
+        >
+          <QRCode
+            value={qrValue}
+            bgColor="transparent"
+            fgColor="var(--chakra-colors-brand-400)"
+          />
+          <VStack alignItems="center" p={4}>
+            <Heading
+              as="h3"
+              fontSize="2xl"
+              textAlign={"center"}
+              color="white"
+              noOfLines={1}
+            >
+              {displayName || "No Name Account"}
+            </Heading>
+            <Text
+              fontFamily="mono"
+              color="brand.400"
+              textAlign={"center"}
+              noOfLines={1}
+            >
+              {accountId}
+            </Text>
+          </VStack>
+        </Flex>
     </VStack>
   );
 }
