64297b83ba6596c1837a7a14cfc08a85efdee2bf
BenKurrek <bkurrek@uwaterloo.ca>
2024-09-10 13:53:53 -0400

    fix build issues

--- src/components/claim/hidden.tsx
+++ src/components/claim/hidden.tsx
@@ -40,14 +40,22 @@ export function Hidden({ foundItem, onReveal }: HiddenProps) {
     },
     trackTouch: true,
     trackMouse: false,
-    preventDefaultTouchmoveEvent: true, // Prevents touchmove event by default
   });
 
   useEffect(() => {
+    // Prevent default touchmove events to stop the page from scrolling or refreshing
+    const preventTouchMove = (e: TouchEvent) => {
+      e.preventDefault();
+    };
+
+    // Add event listener for touchmove to prevent page scroll and refresh
+    document.addEventListener("touchmove", preventTouchMove, {
+      passive: false,
+    });
+
+    // Clean up on component unmount
     return () => {
-      if (requestRef.current) {
-        cancelAnimationFrame(requestRef.current);
-      }
+      document.removeEventListener("touchmove", preventTouchMove);
     };
   }, []);
 
@@ -114,7 +122,6 @@ export function Hidden({ foundItem, onReveal }: HiddenProps) {
         </VStack>
         <Box p={4} width={"100%"}>
           <Box
-            position="relative"
             width={"100%"}
             bg="bg.primary"
             fontFamily={"mono"}
@@ -126,9 +133,17 @@ export function Hidden({ foundItem, onReveal }: HiddenProps) {
             color={"white"}
             fontSize="sm"
             borderRadius={"0"}
-            overflow="hidden"
-            border="2px solid var(--chakra-colors-brand-400)"
-            touchAction="none" // Prevents page shifting or refresh
+            position={"relative"}
+            _before={{
+              content: '""',
+              position: "absolute",
+              width: "57px",
+              height: "37px",
+              left: -4,
+              top: -4,
+              zIndex: -1,
+              background: "black",
+            }}
           >
             {/* Sliding progress */}
             <Box
@@ -141,7 +156,7 @@ export function Hidden({ foundItem, onReveal }: HiddenProps) {
               top={0}
             />
             <Box zIndex={1} position="relative" width="100%">
-              <span>Slide to Reveal & Claim</span>
+              <span>Swipe to reveal and claim</span>
               <ArrowIcon
                 width={24}
                 direction="right"
