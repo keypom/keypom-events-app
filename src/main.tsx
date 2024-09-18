6c3001ac986b81c60028d500ce6920d7dab557fd
Elliot Braem <16282460+elliotBraem@users.noreply.github.com>
2024-09-18 09:40:55 -0400

    remove mock service worker

--- src/main.tsx
+++ src/main.tsx
@@ -1,46 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import { isTestEnv } from "./constants/common";
 
-async function prepare() {
-  // Skip if in test environment
-  if (isTestEnv) {
-    console.log("Skipping mock service worker in test environment");
-    return;
-  }
-
-  console.log("Starting mock service worker");
-  const { worker } = await import("@/mocks/browser");
-
-  return worker.start({
-    serviceWorker: {
-      url: "/mockServiceWorker.js",
-    },
-    onUnhandledRequest(req, print) {
-      // Workaround: passthrough all when in production
-      if (!import.meta.env.DEV) return;
-      // Ignore routing requests
-      if (req.url.startsWith(window.location.origin)) return;
-      // Ignore font requests
-      if (
-        req.url.startsWith("https://fonts.googleapis.com") ||
-        req.url.startsWith("https://fonts.gstatic.com")
-      )
-        return;
-
-      // Ignore RPC requests
-      if (req.url.startsWith("https://rpc.testnet.near.org/")) return;
-
-      print.warning();
-    },
-  });
-}
-
-prepare().then(() => {
-  createRoot(document.getElementById("root")!).render(
-    <StrictMode>
-      <App />
-    </StrictMode>,
-  );
-});
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
