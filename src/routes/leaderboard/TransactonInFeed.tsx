be2d6a448ef48a18d5b2954cc7d283dfa99d63c9
BenKurrek <bkurrek@uwaterloo.ca>
2024-09-25 13:58:48 -0400

    added a dynamic leaderboard which updates every 10 seconds

--- src/routes/leaderboard/TransactonInFeed.tsx
+++ src/routes/leaderboard/TransactonInFeed.tsx
@@ -1,7 +1,7 @@
-// TopEarner.tsx
-import React from "react";
+// TransactionInFeed.tsx
+import React, { useState, useEffect } from "react";
 import { Box, HStack, Flex, Text } from "@chakra-ui/react";
-import { formatTimestamp, getUsername, rewardMessage } from "./helpers"; // Adjust the import path accordingly
+import { formatTimestamp, getUsername, rewardMessage } from "./helpers";
 import { ArrowIcon, UserIcon } from "@/components/icons";
 import { TransactionType } from "./types";
 
@@ -12,7 +12,59 @@ interface TransactionInFeedProps {
 
 export const TransactionInFeed: React.FC<TransactionInFeedProps> = React.memo(
   ({ tx, index }) => {
+    const [timeAgo, setTimeAgo] = useState<string | undefined>();
+
     const isClaim = tx.Claim !== undefined;
+
+    useEffect(() => {
+      let intervalId: NodeJS.Timeout | undefined;
+
+      const updateTimeAgo = () => {
+        const formattedTime = formatTimestamp(tx);
+        setTimeAgo(formattedTime);
+
+        // Check if the time difference is now >= 60 seconds
+        const timestamp = tx.Claim
+          ? tx.Claim.timestamp
+          : tx.Transfer?.timestamp;
+        if (!timestamp) return;
+
+        const timestampInMs = timestamp / 1e6;
+        const now = Date.now();
+        const diffMs = now - timestampInMs;
+        const diffSeconds = Math.floor(diffMs / 1000);
+
+        if (diffSeconds >= 60 && intervalId) {
+          clearInterval(intervalId);
+          intervalId = undefined;
+        }
+      };
+
+      updateTimeAgo(); // Initial call
+
+      // Set up interval if the transaction is less than 60 seconds old
+      const timestamp = tx.Claim ? tx.Claim.timestamp : tx.Transfer?.timestamp;
+      if (!timestamp) return;
+
+      const timestampInMs = timestamp / 1e6;
+      const now = Date.now();
+      const diffMs = now - timestampInMs;
+      const diffSeconds = Math.floor(diffMs / 1000);
+
+      if (diffSeconds < 60) {
+        intervalId = setInterval(updateTimeAgo, 1000);
+      } else {
+        setTimeAgo(formatTimestamp(tx)); // Set initial timeAgo for older transactions
+      }
+
+      // Clean up the interval when the component unmounts
+      return () => {
+        if (intervalId) {
+          clearInterval(intervalId);
+        }
+      };
+    }, [tx]);
+
     return (
       <Box
         key={index}
@@ -56,7 +108,7 @@ export const TransactionInFeed: React.FC<TransactionInFeedProps> = React.memo(
             </Text>
             <ArrowIcon color="white" width={20} />
             <Text fontWeight="700" color="brand.600">
-              {formatTimestamp(tx)}
+              {timeAgo}
             </Text>
           </HStack>
 
