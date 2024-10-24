4df049c213d824cd09f0549afe0f3429b8ffc3fd
BenKurrek <bkurrek@uwaterloo.ca>
2024-10-24 16:45:46 -0400

    started working on new interface for new airtable

--- src/components/agenda/card.tsx
+++ src/components/agenda/card.tsx
@@ -2,15 +2,27 @@ import { AgendaItem } from "@/lib/api/agendas";
 import { useAddToCalendar } from "@/stores/add-to-calendar";
 import { Button, Flex, Heading, Text, VStack } from "@chakra-ui/react";
 import { CalenderAddIcon } from "../icons";
+import { useState } from "react";
 
 export function AgendaCard(event: AgendaItem) {
   const { title, stage, description, presenter } = event;
   const { onOpen, setEvent } = useAddToCalendar();
 
+  const [isExpanded, setIsExpanded] = useState(false);
+
   const handleCalendarClick = () => {
     onOpen();
     setEvent(event);
   };
+
+  const maxChars = 150;
+
+  const shouldTruncate = description.length > maxChars;
+
+  const displayedDescription =
+    shouldTruncate && !isExpanded
+      ? description.slice(0, maxChars) + "..."
+      : description;
 
   return (
     <VStack width={"100%"} alignItems={"flex-start"} gap={2}>
@@ -34,7 +46,7 @@ export function AgendaCard(event: AgendaItem) {
             {presenter}
           </Heading>
         </VStack>
-        <Button variant={"transparent"} onClick={() => handleCalendarClick()}>
+        <Button variant={"transparent"} onClick={handleCalendarClick}>
           <CalenderAddIcon
             width={24}
             height={24}
@@ -43,7 +55,18 @@ export function AgendaCard(event: AgendaItem) {
         </Button>
       </Flex>
       <Text color={"brand.600"} fontSize={"xs"}>
-        {description}
+        {displayedDescription}
+        {shouldTruncate && (
+          <Button
+            variant="link"
+            color="brand.400"
+            size="xs"
+            onClick={() => setIsExpanded(!isExpanded)}
+            ml={1}
+          >
+            {isExpanded ? "View Less" : "View More"}
+          </Button>
+        )}
       </Text>
       <Heading as="h4" color={"brand.400"} fontSize={"sm"} fontWeight={"bold"}>
         {stage}
